b7db7a454d0ada20bb1ee0c24e7a059155b56ca2
GuiPoZhou <1207336828@qq.com>
2023-11-13 14:33:10 +0800

    pinia init

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -5,10 +5,13 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@renderer/assets/css/index.scss'
+import { createPinia } from 'pinia'
+
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(ElementPlus)
 app.use(router)
 app.use(store)
-
+app.use(pinia)
 app.mount('#app')
